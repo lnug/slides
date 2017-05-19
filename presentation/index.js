445e1996457cda9f49de1ad359f298de98ba5b80
Clarkie <andrew.t.clarke@gmail.com>
2017-05-19 06:29:48 +0100

    feat: finishing porting from deck

--- presentation/index.js
+++ presentation/index.js
@@ -33,7 +33,8 @@ const images = {
   makersLogo: require("../assets/makers_logo.png"),
   tiroLogo: require("../assets/tiro_logo.jpg"),
   pusherLogo: require("../assets/pusher_logo_white.png"),
-  pizza: require("../assets/pizza-1.gif")
+  pizza: require("../assets/pizza-1.gif"),
+  cheers: require("../assets/cheers.gif")
 };
 
 preloader(images);
@@ -49,7 +50,7 @@ const theme = createTheme({
 
 const speakers = [
   { name: "Paul Jensen", title: "From LNUG presentation to published book" },
-  null,
+  { name: "", title: "Trading cryptocurrencies, forex, commodities stocks and more using node.js" },
   { name: "Will Munn", title: "Speeding up CI with node and docker" }
 ];
 
@@ -138,7 +139,72 @@ export default class Presentation extends React.Component {
           <Heading size={5} textColor="secondary">Have something to share?</Heading>
         </Slide>
 
+        <Slide>
+          <Heading size={3} textColor="secondary">Get Involved</Heading>
+          <List>
+            <ListItem>Assets & Slides</ListItem>
+            <List>
+              <ListItem>github.com/lnug/resources</ListItem>
+            </List>
+            <ListItem>Website Tips</ListItem>
+            <List>
+              <ListItem>github.com/lnug/lnug.github.io</ListItem>
+            </List>
+            <ListItem>Gitter Channel</ListItem>
+            <List>
+              <ListItem>lnug/discuss</ListItem>
+            </List>
+          </List>
+        </Slide>
+
+        <Slide>
+          <Heading size={3} textColor="secondary">Call for speakers</Heading>
+          <List>
+            <ListItem>We have speaker slots available!</ListItem>
+            <ListItem>Visit github.com/lnug/speakers to submit your talk!</ListItem>
+          </List>
+        </Slide>
+
+        <Slide>
+          <Heading size={3} textColor="secondary">Next Time</Heading>
+          <Heading size={4} textColor="secondary">26th April 2017</Heading>
+          <Text>
+            The 4th Wednesday of the month
+            meetup.com/london-nodejs/
+          </Text>
+          <List>
+            <ListItem>Rubbish in Bins</ListItem>
+            <ListItem>Stack chairs against walls</ListItem>
+            <ListItem>Get involved!</ListItem>
+          </List>
+        </Slide>
+
+        <Slide>
+          <Heading size={3} textColor="secondary">After Party</Heading>
+          <Heading size={5} textColor="secondary">The Culpeper</Heading>
+          <BlockQuote>
+            40 Commercial Street,
+            London,
+            E1 6LP
+          </BlockQuote>
+          <Text>http://theculpeper.com/pub/</Text>
+          <Image src={images.cheers.replace("/", "")} margin="0px auto 40px" height="293px"/>
+        </Slide>
+
         <SpeakerSlide speaker={speakers[2]} />
+
+        <Slide>
+          <Heading size={3} textColor="secondary">Thank You</Heading>
+          <Heading size={5} textColor="secondary">Had a great time? Let us know!</Heading>
+          <Text>github.com/lnug/feedback</Text>
+          <Heading size={5} textColor="secondary">Brought to you by</Heading>
+          <List>
+            <ListItem>Makers Academy</ListItem>
+            <ListItem>Tiro Partners</ListItem>
+            <ListItem>Pusher</ListItem>
+            <ListItem>github.com/orgs/lnug/people</ListItem>
+          </List>
+        </Slide>
 
       </Deck>
     );

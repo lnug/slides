fb101af63bd5b62789e7573f9a413354861ba620
Cyril Silverman <silverwolf90@gmail.com>
2017-05-24 16:51:52 +0100

    make them vaguely passable

--- presentation/index.js
+++ presentation/index.js
@@ -1,16 +1,13 @@
 // Import React
 import React from "react";
-const { PropTypes } = React;
 
 // Import Spectacle Core tags
 import {
   BlockQuote,
-  Cite,
   Deck,
   Heading,
   ListItem,
   List,
-  Quote,
   Slide,
   Text,
   Image,
@@ -27,6 +24,7 @@ import createTheme from "spectacle/lib/themes/default";
 require("normalize.css");
 require("spectacle/lib/themes/default/index.css");
 
+const { PropTypes } = React;
 
 const images = {
   logo: require("../assets/lnug-logo.svg"),
@@ -47,10 +45,14 @@ const theme = createTheme({
   secondary: "Helvetica"
 });
 
+const emcee = {
+  name: "Cyril",
+  twitter: "cyril.silverman"
+};
 
 const speakers = [
   { name: "Paul Jensen", title: "From LNUG presentation to published book" },
-  { name: "", title: "Trading cryptocurrencies, forex, commodities stocks and more using node.js" },
+  { name: "flyingunicorn222", title: "Trading cryptocurrencies, forex, commodities stocks and more using node.js" },
   { name: "Will Munn", title: "Speeding up CI with node and docker" }
 ];
 
@@ -65,45 +67,43 @@ class SpeakerSlide extends React.Component {
     if (!speaker) {
       return null;
     }
+
     return (
       <Slide>
-        <Heading size={3} textColor="secondary">Next Up</Heading>
+        <Heading size={5} textColor="secondary" margin={50}>Next Up</Heading>
         <Heading size={4} textColor="secondary">{speaker.name}</Heading>
-        <Heading size={5} textColor="secondary">{speaker.title}</Heading>
-        <Text>Grab a drink & pizza then grab a seat!</Text>
-        <Image src={images.pizza.replace("/", "")} margin="0px auto 40px" height="293px"/>
+        <Heading size={6} textColor="secondary"><S type="italics">{speaker.title}</S></Heading>
+        <Text textSize={30} margin={50} textColor="secondary">Grab a drink & pizza then grab a seat!</Text>
       </Slide>
     );
   }
 }
-
-const speakerListItem = (speaker) => (
-  speaker
-  ? <ListItem key={speaker.title}>{speaker.title} <S type="italics">- {speaker.name}</S></ListItem>
-  : null
-);
 
 export default class Presentation extends React.Component {
   render() {
     return (
       <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
         <Slide transition={["zoom"]} bgColor="primary">
-          <Image src={images.logo} margin="0px auto 40px" height="293px"/>
-          <Heading size={3} textColor="secondary">DATE (#NUMBER)</Heading>
+          <Image src={images.logo} margin="0px auto 0px" height="200px"/>
+          <Heading size={6} textColor="secondary">May 24th, 2017 (#63)</Heading>
           <List>
-            {speakers.map(speakerListItem)}
+            {speakers.map((speaker) =>
+              <ListItem textSize={30} key={speaker.title}>
+                {speaker.title} <S type="italics">- {speaker.name}</S>
+              </ListItem>
+            )}
           </List>
           <List>
-            <ListItem>WIFI: See the walls</ListItem>
-            <ListItem>Twitter: @LNUGorg use hashtags #LNUG #node #javascript #london</ListItem>
-            <ListItem>Gitter: lnug/discuss</ListItem>
+            <ListItem textSize={30}>WIFI: See the walls</ListItem>
+            <ListItem textSize={30}>Twitter: @LNUGorg use hashtags #LNUG #node #javascript #london</ListItem>
+            <ListItem textSize={30}>Gitter: lnug/discuss</ListItem>
           </List>
         </Slide>
 
         <Slide transition={["fade"]} bgColor="primary">
           <Heading size={3} textColor="secondary">House Keeping</Heading>
           <List>
-            <ListItem>Say hello on Twitter: @lnugorg #lnug (and me: <S type="bold">@clarkieclarkie</S>)</ListItem>
+            <ListItem>Say hello on Twitter: @lnugorg #lnug (and me: <S type="bold">@{emcee.twitter}</S>)</ListItem>
             <ListItem>YouTube: LNUG Team 🎥</ListItem>
             <ListItem>Instagram: <S type="bold">@lnugorg</S> #lnug 📷</ListItem>
             <ListItem>Gitter: <S type="bold">lnug/discuss</S> 📢</ListItem>
@@ -132,8 +132,8 @@ export default class Presentation extends React.Component {
         <SpeakerSlide speaker={speakers[1]} />
 
         <Slide>
-          <Heading size={3} textColor="secondary">Community Announcements</Heading>
-          <Text>Who? What? Contact details?</Text>
+          <Heading size={3} textColor="secondary" margin={50}>Community Announcements</Heading>
+          <Heading size={5} textColor="secondary">Who? What? Contact details?</Heading>
           <Heading size={5} textColor="secondary">Hiring?</Heading>
           <Heading size={5} textColor="secondary">Available for hire?</Heading>
           <Heading size={5} textColor="secondary">Have something to share?</Heading>
@@ -142,18 +142,9 @@ export default class Presentation extends React.Component {
         <Slide>
           <Heading size={3} textColor="secondary">Get Involved</Heading>
           <List>
-            <ListItem>Assets & Slides</ListItem>
-            <List>
-              <ListItem>github.com/lnug/resources</ListItem>
-            </List>
-            <ListItem>Website Tips</ListItem>
-            <List>
-              <ListItem>github.com/lnug/lnug.github.io</ListItem>
-            </List>
-            <ListItem>Gitter Channel</ListItem>
-            <List>
-              <ListItem>lnug/discuss</ListItem>
-            </List>
+            <ListItem>Assets & Slides<br/>(<S type="bold">github.com/lnug/resources</S>)</ListItem>
+            <ListItem>Website Tips<br/>(<S type="bold">github.com/lnug/lnug.github.io</S>)</ListItem>
+            <ListItem>Gitter Channel<br/>(<S type="bold">lnug/discuss</S>)</ListItem>
           </List>
         </Slide>
 
@@ -161,16 +152,18 @@ export default class Presentation extends React.Component {
           <Heading size={3} textColor="secondary">Call for speakers</Heading>
           <List>
             <ListItem>We have speaker slots available!</ListItem>
-            <ListItem>Visit github.com/lnug/speakers to submit your talk!</ListItem>
+            <ListItem>Visit <S type="bold">github.com/lnug/speakers</S> to submit your talk!</ListItem>
           </List>
         </Slide>
 
         <Slide>
-          <Heading size={3} textColor="secondary">Next Time</Heading>
-          <Heading size={4} textColor="secondary">26th April 2017</Heading>
-          <Text>
+          <Heading size={3} textColor="secondary" margin={40}>Next Time</Heading>
+          <Heading size={4} textColor="secondary">18th June 2017</Heading>
+          <Text textColor="secondary" bold>
+            meetup.com/london-nodejs
+          </Text>
+          <Text textColor="secondary" italic margin={20}>
             The 4th Wednesday of the month
-            meetup.com/london-nodejs/
           </Text>
           <List>
             <ListItem>Rubbish in Bins</ListItem>
@@ -180,15 +173,15 @@ export default class Presentation extends React.Component {
         </Slide>
 
         <Slide>
-          <Heading size={3} textColor="secondary">After Party</Heading>
+          <Heading size={3} textColor="secondary" margin={30}>After Party</Heading>
           <Heading size={5} textColor="secondary">The Culpeper</Heading>
-          <BlockQuote>
-            40 Commercial Street,
-            London,
-            E1 6LP
-          </BlockQuote>
-          <Text>http://theculpeper.com/pub/</Text>
-          <Image src={images.cheers.replace("/", "")} margin="0px auto 40px" height="293px"/>
+          <Text textColor="secondary" margin={10}>
+            40 Commercial Street, London, E1 6LP
+          </Text>
+          <Text textColor="secondary" margin={10}>
+            http://theculpeper.com/pub/
+          </Text>
+          <Image src={images.cheers.replace("/", "")} margin="40px auto 0px" height="200px"/>
         </Slide>
 
         <SpeakerSlide speaker={speakers[2]} />
@@ -196,9 +189,9 @@ export default class Presentation extends React.Component {
         <Slide>
           <Heading size={3} textColor="secondary">Thank You</Heading>
           <Heading size={5} textColor="secondary">Had a great time? Let us know!</Heading>
-          <Text>github.com/lnug/feedback</Text>
-          <Heading size={5} textColor="secondary">Brought to you by</Heading>
-          <List>
+          <Text textColor="secondary">github.com/lnug/feedback</Text>
+          <Heading size={5} textColor="secondary" margin={"40px 0 0 0"}>Brought to you by</Heading>
+          <List margin="20px 0 0 0">
             <ListItem>Makers Academy</ListItem>
             <ListItem>Tiro Partners</ListItem>
             <ListItem>Pusher</ListItem>
